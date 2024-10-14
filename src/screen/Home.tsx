8049c8807be5c6c6a7d1e77a29de11b2a39abc40
eoqna <eoqna@naver.com>
2024-10-14 23:00:48 +0900

    Home 화면 UI 적용

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,18 +1,73 @@
 import styled from "styled-components";
 import { NavigationProps } from "../navigation";
 import { color } from "../utils/colors";
+import img1 from "../assets/imgs/cat1.png";
 
 const Layout = styled.div`
-  height: 100%;
-  width: 85%;
+  height: calc(100% - 40px);
+  width: calc(85% - 40px);
   background: ${color.LightPink};
+  padding: 20px;
+`;
+
+const GridLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+`;
+
+const ImgLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  margin-bottom: 10px;
+  transition: all 0.2s ease-in;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const Title = styled.p`
+  font-size: 3vmin;
+  font-weight: bold;
+  color: ${color.White};
 `;
 
 const Home = (props: NavigationProps) => {
   const { navigation } = props;
   
   return (
-    <Layout></Layout>
+    <Layout>
+      <GridLayout>
+        <ImgLayout>
+          <Image src={img1} alt="cat1" />
+          <Title>cat 1</Title>
+        </ImgLayout>
+        <ImgLayout>
+          <Image src={img1} alt="cat2" />
+          <Title>cat 2</Title>
+        </ImgLayout>
+        <ImgLayout>
+          <Image src={img1} alt="cat3" />
+          <Title>cat 3</Title>
+        </ImgLayout>
+        <ImgLayout>
+          <Image src={img1} alt="cat4" />
+          <Title>cat 4</Title>
+        </ImgLayout>
+        <ImgLayout>
+          <Image src={img1} alt="cat5" />
+          <Title>cat 5</Title>
+        </ImgLayout>
+      </GridLayout>
+    </Layout>
   );
 };
 
